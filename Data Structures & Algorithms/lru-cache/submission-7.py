78f8501bcc4e56f4d4250dcfa589e46264a8a3c3
Hussein <181645188+HusseinAshraf10@users.noreply.github.com>
2026-09-12 09:29:11 +0300

    Add: lru-cache - submission-7

---
+++ Data Structures & Algorithms/lru-cache/submission-7.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, key = 0, val = 0):
+        self.key, self.val = key, val
+        self.next, self.prev = None, None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.map = {}
+        self.head, self.tail = Node(), Node()
+        self.head.next, self.tail.prev = self.tail, self.head
+        
+    def remove(self, node):
+        node.next.prev, node.prev.next = node.prev, node.next
+
+    def insert(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key in self.map:
+            node = self.map[key]
+            self.remove(node)
+            self.insert(node)
+            return node.val
+        return -1
+
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.map:
+            node = self.map[key]
+            node.val = value
+            self.remove(node)
+            self.insert(node)
+        else:
+            if len(self.map) == self.cap:
+                lru = self.tail.prev
+                self.remove(lru)
+                del self.map[lru.key]
+            new_node = Node(key, value)
+            self.map[key] = new_node
+            self.insert(new_node)
+
+
+
